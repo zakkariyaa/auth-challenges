eb11b56960f6f31785f9949e9e6de52bd55a7f4c
Zakarie Yaris <zakariaali350@gmail.com>
2023-05-27 11:39:41 +0100

    Initial commit

---
+++ src/routes/log-out.js
@@ -0,0 +1,11 @@
+function post(req, res) {
+  /**
+   * [1] Get the session ID from the cookie
+   * [2] Remove that session from the DB
+   * [3] Remove the session cookie
+   * [4] Redirect back home
+   */
+  res.status(500).send("");
+}
+
+module.exports = { post };
